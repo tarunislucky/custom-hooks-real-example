8e5745c2e9b86eb175723fa764b8ad5b796c7edf
Laxmi Prasanna <laxmiprasanna59@gmail.com>
2023-06-15 17:27:28 +0530

    created useHttp custom hook and used it in App

--- src/App.js
+++ src/App.js
@@ -2,38 +2,28 @@ import React, { useEffect, useState } from 'react';
 
 import Tasks from './components/Tasks/Tasks';
 import NewTask from './components/NewTask/NewTask';
+import useHttp from './hooks/use-http';
 
 function App() {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
   const [tasks, setTasks] = useState([]);
 
-  const fetchTasks = async (taskText) => {
-    setIsLoading(true);
-    setError(null);
-    try {
-      const response = await fetch(
-        'https://react-http-485a4-default-rtdb.firebaseio.com/tasks.json'
-      );
+  const transformTasks = tasksOObj => {
+    const loadedTasks = [];
 
-      if (!response.ok) {
-        throw new Error('Request failed!');
-      }
-
-      const data = await response.json();
-
-      const loadedTasks = [];
-
-      for (const taskKey in data) {
-        loadedTasks.push({ id: taskKey, text: data[taskKey].text });
-      }
-
-      setTasks(loadedTasks);
-    } catch (err) {
-      setError(err.message || 'Something went wrong!');
+    for (const taskKey in tasksOObj) {
+      loadedTasks.push({ id: taskKey, text: tasksOObj[taskKey].text });
     }
-    setIsLoading(false);
-  };
+
+    setTasks(loadedTasks);
+  }
+
+  // useHttp is a custom hook that takes a configuration object and a function to handle the results
+  // it returns isLoding state, error state, and a function that can be used to send fetch request 
+  // the function will also call the handler we earlier passed to useHttp with the result data
+
+  const { isLoading, error, sendRequest: fetchTasks } = useHttp({
+    url: 'https://react-http-485a4-default-rtdb.firebaseio.com/tasks.json'
+  }, transformTasks);
 
   useEffect(() => {
     fetchTasks();
